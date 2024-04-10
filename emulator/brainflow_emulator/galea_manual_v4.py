e1ed71d01685036989a1bff6c5a88ef619e4de48
Richard Waltman <richard@openbci.com>
2024-04-10 14:44:21 -0500

    Add other register command strings to Galea v4 Emulator F1, F2, and F3

--- emulator/brainflow_emulator/galea_manual_v4.py
+++ emulator/brainflow_emulator/galea_manual_v4.py
@@ -15,7 +15,7 @@ class State(enum.Enum):
 class Message(enum.Enum):
     start_stream = b'b'
     stop_stream = b's'
-    ack_values = (b'd', b'~6', b'~5', b'~4', b'o', b'F0')
+    ack_values = (b'd', b'~6', b'~5', b'~4', b'o', b'F0', b'F1', b'F2', b'F3')
     ack_from_device = b'A'
     time_calc_command = b'F4444444'
 
